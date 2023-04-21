4662ee96c8fd663570666ba51ec8ea01b0cd0aac
EmyleSantos <emylesantoslima@gmail.com>
2023-04-21 20:20:18 -0300

    criei funções para listar e cadastrar turmas

--- server/config/express-config.js
+++ server/config/express-config.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 
-const userRouter = require('./usuario');
-const classRouter = require('./turma');
+const turmaRouter = require('../src/domains/turma/controllers/index.js');
 
-app.use('/usuario', userRouter);
-app.use('/turma', classRouter);
+app.use('/', turmaRouter);
+app.use('/turmas', turmaRouter);
+app.use('/turmas/cadastrar', turmaRouter);
+app.use('/turmas/nova', turmaRouter);
+
 
 const PORT = 3000;
 app.listen(PORT, console.log("Server is running on port 3000"));
--- server/src/domains/turma/services/TurmaService.js
+++ server/src/domains/turma/services/TurmaService.js
@@ -1,10 +1,66 @@
 // Configuração de todos os metodos que a entidade Turma precisa no sistema.
+const Turma = require('../models/Turma').Turma;
+const Usuario = require('../../usuario/models/Usuario').Usuario;
 
-async function addTurma() {
+
+async function addTurma( novaTurma ) {
     // Verifica se turma existe
-
-    // Verifica se turma já não está cadastrada
+    console.log("Buscando turmas...")
+    var turma = await Turma.findOne({ 
+        codigo: novaTurma.codigo,
+        turma: novaTurma.turma
+    }).exec();
+    
+    if (turma == null) {
+        console.log("Turma não existe!");
+        throw new Error('Turma informada não existe!');
+    } 
+    
+    // Verifica se turma já foi cadastrada para este usuário
+    var turmaUser = await Usuario.find({
+        nome: "Usuário Padrão",
+        "turmas.codigo": novaTurma.codigo,
+        "turmas.turma": novaTurma.turma
+    }).exec();
+    
+    if (turmaUser.length > 0) {
+        console.log("Turma já cadastrada");
+        throw new Error('Turma já cadastrada pelo usuário!');
+    }
 
     // Cadastra turma
+    try {
+        turma.set({ faltas: novaTurma.faltas });
+    } catch {
+        turma.set({ faltas: 0 });
+    }
+
+    turma.turma = novaTurma.turma;
+
+    await Usuario.updateOne(
+        { nome: "Usuário Padrão" },
+        { $push: { turmas: turma } },
+    ).then(() => {
+        console.log("Turma cadastrada com sucesso!")
+    });
     
-}
+};
+
+async function getListaTurmas() {
+    try {
+        const usuario = await Usuario.findOne({ nome: "Usuário Padrão" }).exec();
+
+        if (usuario == null) {
+            throw new Error('Não foi possível encontrar o usuário!');
+        }
+
+        const turmas = usuario.turmas;
+        return turmas;
+
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+module.exports.addTurma = addTurma;
+module.exports.getListaTurmas = getListaTurmas;
